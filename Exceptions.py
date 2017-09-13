aeafa2703f3af6b062509de923e04d64daf23dcb
Youssef Ali <youssefali6212@gmail.com>
2017-09-13 23:52:35 +0300

    Create Exceptions.py

---
+++ Exceptions.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for i in range(t):
+    a,b = input().split()
+    try:
+        print(int(a)//int(b))
+    except ZeroDivisionError as e:
+        print("Error Code:",e)
+    except ValueError as e:
+        print("Error Code:",e)
